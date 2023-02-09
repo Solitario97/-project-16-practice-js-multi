96b360cc7903d8e836724dc0a608d66dc982b17a
Solitario97 <a.nurmagambetov97@gmail.com>
2023-02-09 07:41:55 +0600

    i one

--- js/02-accordeon.js
+++ js/02-accordeon.js
@@ -1,7 +1,19 @@
  const headers = document.querySelectorAll('[data-name="accordeon-title"]'); 
 
+ const contentBox = document.querySelectorAll('.list-content');
+
+
+
+
  headers.forEach(function (item){
+
+    contentBox.forEach(function(item) {
+        item.classList.add('hidden');
+     });
+
     item.addEventListener('click', function () {
         this.nextElementSibling.classList.toggle('hidden');
+        this.previousElementSibling.classList.add('hidden');
     });
+
  });
